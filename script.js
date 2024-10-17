0da88ab380ec824e6a8fefe76fa3e98c1cd8af8f
Mayank Padhi <mayankpadhi91@gmail.com>
2024-10-17 12:19:27 +0530

    1 stage

---
+++ script.js
@@ -0,0 +1,39 @@
+const key = "d2dfe6a0bd56d7697025ad7e6be54d6";
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let cityName = document.getElementById("city");
+
+let getWeather = () => {
+  let cityValue = cityName.value;
+  if (cityValue.length == 0) {
+    result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
+  }
+  else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}f&units=metric`;
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        result.innerHTML = `
+        <h2>${data.name}</h2>
+        <h4 class="weather">${data.weather[0].main}</h4>
+        <h4 class="desc">${data.weather[0].description}</h4>
+        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+        <h1>${data.main.temp} &#176;</h1>
+        <div class="temp-container">
+            <div>
+                <h4 class="title">min</h4>
+                <h4 class="temp">${data.main.temp_min}&#176;</h4>
+            </div>
+            <div>
+                <h4 class="title">max</h4>
+                <h4 class="temp">${data.main.temp_max}&#176;</h4>
+            </div>
+        </div>
+        `;
+      })
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">City not found</h3>`;
+      });
+  }
+};
+searchBtn.addEventListener("click", getWeather);    
